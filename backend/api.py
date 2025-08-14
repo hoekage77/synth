8641889455e92426d73321926e3de47903cba2ec
Jesse <jessenath8@gmail.com>
2025-08-14 10:15:15 +0000

    domup

--- backend/api.py
+++ backend/api.py
@@ -130,18 +130,18 @@ async def log_requests_middleware(request: Request, call_next):
         raise
 
 # Define allowed origins based on environment
-allowed_origins = ["https://www.suna.so", "https://suna.so"]
+allowed_origins = ["https://astril.com.ng", "https://hoekage.tech"]
 allow_origin_regex = None
 
 # Add staging-specific origins
 if config.ENV_MODE == EnvMode.LOCAL:
-    allowed_origins.append("http://localhost:3000")
+    allowed_origins.append("http://astril.com.ng")
 
 # Add staging-specific origins
 if config.ENV_MODE == EnvMode.STAGING:
-    allowed_origins.append("https://staging.suna.so")
+    allowed_origins.append("https://stage.astril.com.ng")
     allowed_origins.append("http://localhost:3000")
-    allow_origin_regex = r"https://suna-.*-prjcts\.vercel\.app"
+    allow_origin_regex = r"https://xera-.*-prjcts\.vercel\.app"
 
 app.add_middleware(
     CORSMiddleware,
